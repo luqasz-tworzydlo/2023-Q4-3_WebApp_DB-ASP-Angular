74334069bb40bf2b248bd96dcd11b1febc6476a1
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2023-12-30 17:16:30 +0100

    Add files via upload

--- 1_MS_AN (API) [1]/src/LTL_TDLA/LuqTwoList_ToDoListApp/LuqTwoList_ToDoListApp/Program.cs
+++ 1_MS_AN (API) [1]/src/LTL_TDLA/LuqTwoList_ToDoListApp/LuqTwoList_ToDoListApp/Program.cs
@@ -1,9 +1,12 @@
+using LuqTwoList_ToDoListApp.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
+builder.Services.AddDbContext<ToDoListContext>(opt => opt.UseSqlServer(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
